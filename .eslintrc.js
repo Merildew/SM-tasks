fed0aec3a985af527a571ef2c43700885df56606
Valeriya Nekrashevich <73027852+Merildew@users.noreply.github.com>
2022-02-18 03:25:43 +0300

    Create project. Add header & top info panel

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+      jquery: true,
+    },
+    extends: ["eslint:recommended", "google", "prettier"],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      "require-jsdoc": 0,
+    },
+  };
